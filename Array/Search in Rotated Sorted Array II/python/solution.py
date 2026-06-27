d8779f1bdc521304f5eedb0f8d76bb3bb02f0f67
R.P.Nishwa <nishwapandiyan@gmail.com>
2026-06-27 22:23:38 +0530

    feat: Add Search in Rotated Sorted Array II

--- Array/Search in Rotated Sorted Array II/python/solution.py
+++ Array/Search in Rotated Sorted Array II/python/solution.py
@@ -1,21 +1,26 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
         low = 0
-        high = len(nums)-1
-        
+        high = len(nums) - 1
+
         while low <= high:
-            mid = low+(high-low)//2
+            mid = low + (high - low) // 2
 
             if nums[mid] == target:
                 return True
-            if nums[low] <= target < nums[mid]:
-                if nums[mid] < target:
-                    high = mid-1
+
+            
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target < nums[mid]:
+                    high = mid - 1
                 else:
-                    low = mid+1
+                    low = mid + 1
+
+            
             else:
                 if nums[mid] < target <= nums[high]:
-                    low = mid+1
+                    low = mid + 1
                 else:
-                    high = mid-1
-        return False                                
+                    high = mid - 1
+
+        return False

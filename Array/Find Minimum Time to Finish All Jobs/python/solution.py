46f404a949ebd8bb2e5199e2df293d7b87e16565
R.P.Nishwa <nishwapandiyan@gmail.com>
2026-06-29 12:48:58 +0530

    feat: Add Find Minimum Time to Finish All Jobs

--- Array/Find Minimum Time to Finish All Jobs/python/solution.py
+++ Array/Find Minimum Time to Finish All Jobs/python/solution.py
@@ -3,14 +3,29 @@ class Solution:
         low = max(jobs)
         high = sum(jobs)
 
+        def canWork(limit):
+            workers = [0]*k
+            def dfs(index):
+                if index == len(jobs):
+                    return True
+                for i in range(k):
+                    if workers[i] + jobs[index] <= limit:
+                        workers[i] += jobs[index] 
+
+                        if dfs(index+1):
+                            return True
+                        workers[i] -= jobs[index]
+                    if workers[i] == 0:
+                        break
+                return False        
+
+                
+            return dfs(0)  
+
         while low < high:
             mid = low+(high-low)//2
             if canWork(mid):
                 high = mid
             else:
-                low = mid+1
-        def canWork(limit):
-            workers = [0]*k
-            def dfs(index):
-                
-        return dfs(0)                     
+                low = mid+1   
+        return low                    
